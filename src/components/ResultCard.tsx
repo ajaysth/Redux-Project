22c05a19f7304f52f49d98c2d94f132af51ff079
ajaysth <shrestha11ajay@gmail.com>
2026-02-22 16:13:40 +0545

    collection page yt to created

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -10,6 +10,14 @@ interface Props {
     item: ItemProps
 }
 const ResultCard = ({ item }: Props) => {
+    const addToCollection = (item: ItemProps) => {
+        const olddata = JSON.parse(localStorage.getItem("collection") || "[]");
+        const newdata = [...olddata, item]
+        localStorage.setItem("collection", JSON.stringify(newdata))
+        console.log(newdata);
+
+
+    }
     return (
         <div className=" cursor-pointer group relative w-80 h-80 rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition-all duration-300 bg-gray-900">
 
@@ -44,11 +52,11 @@ const ResultCard = ({ item }: Props) => {
             <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent" />
 
             {/* TITLE */}
-            <div className="absolute bottom-0 p-4">
+            <div className="flex justify-between absolute bottom-0 p-4">
                 <h2 className="text-white font-semibold text-lg line-clamp-2">
                     {item.title}
                 </h2>
-                <button className="mt-2 px-4 py-2 bg-white text-black rounded-lg hover:bg-gray-200 transition-colors duration-300">
+                <button onClick={() => { addToCollection(item) }} className="m-2 active:scale-95 px-4 py-1 bg-white text-black rounded-lg hover:bg-gray-200 transition-colors duration-300">
                     Save
                 </button>
             </div>
